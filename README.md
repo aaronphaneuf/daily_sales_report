6e4e2b4d0f335560395ebb66ae9a65853bf58577
roux <aaronphaneuf@pm.me>
2019-07-31 17:23:15 -0600

    updated run_report.py, readme

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Daily Sales Report - SQL Query
 
-Pulls from a table named v_TJTrans for each store in the company and summarizes yesterday's sales along with the matching day for last year, week to date, and matching week to date for last year.
+Pulls from a SQL table named v_TJTrans for each store in the company and summarizes yesterday's sales along with the matching day for last year, week to date, and matching week to date for last year.
+
+# daily_sales.sql
+
+<code>SELECT * from v_TJTrans</code> results in over 20 million rows and 38 columns - over 760 million cells of data dating back to 2013.
+Net sales are based off Gross sales - Combos - Discounts. These are obtained through the following filters:
+
+| Column | WHERE Clause | Description |
+|-------------------------------------|
+| TRN_STO_FK | None | All stores are parsed |
+| TRN_AllVoid | = 0 | Void transactions are not included - Primary key |
+| TRN_Void | = 0 | Void transactions are not included - Foreign key |
+| DPT_Name | not like '%99%' | Bottle refunds are not included |
+| TLI_LIT_FK | = 4 | Discount line items included |
+| TLI_LIT_FK | = 35 | Combo line items included |
+| TLI_LIT_FK | = 2 | Unique transactions |
+|TRN_EndTime | variable | TRN_EndTime is used over TRN_StartTime, which is not maintained |
 
 Week to date sales are calculated from Monday - Sunday for each week.
 
-run_report.py runs daily_sales.sql, writes the results to a file and iterates through the data, writing to a table which is then emailed out to certain individuals in the company.
+run_report.py executes daily_sales.sql, writes the results to a file and iterates through the data, writing to a table which is then emailed out to certain individuals in the company on a daily basis.
+
+# run_report.py
+
+Executes daily_sales.sql, which writes to daily_sales.txt. Iterates through the text file and writes the individual store data to dictionaries using a for loop. Stores are accessed through key values and written between <td> tags and sent using smtp.
--- run_report.py
+++ run_report.py
@@ -1,7 +1,4 @@
-import os
-import time
-import smtplib
-import csv
+import os, time, smtplib, csv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import date, timedelta
@@ -10,32 +7,24 @@ yesterday_date = date.today() - timedelta(days=1)
 yesterday_date.strftime('yyyy%m%d')
 yesterday_date_string = str(yesterday_date)
 
-#sent from address
-message_from = "aphaneuf@planetorganic.ca"
-#recipients address
-message_to = "aphaneuf@planetorganic.ca"
+#sent from and sent to email addresses
+message_from = "user@host.com"
+message_to = "user@host.com"
 
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
-#email title
 msg['Subject'] = "Daily Sales Report - " +yesterday_date_string
 msg['From'] = message_from
 msg['To'] = message_to
 
 #runs daily_sales.sql
-os.system('dbisql -c dsn=prototype C:/Users/aphaneuf/Desktop/daily_sales.sql')
-
-#waits one second after the report has been written to daily_sales.txt
-#this may not be needed
-time.sleep(1)
+os.system('dbisql -c dsn=prototype path_to_daily_sales.sql')
 
 #Opens daily_sales.txt and iterates through it
-with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
-
+with open('path_to_daily_sales.txt') as csv_file:
+    #declaring dictionaries for sum of store sales and same store sales
     csv_reader = csv.reader(csv_file, delimiter=',')
     stores = {"22":"Edmonton Ellerslie", "10":"Calgary Classic", "3":"Calgary Shepard", "14":"Calgary Royal Oak", "27":"Edmonton Sherwood", "12": "Edmonton Classic", "9":"Victoria", "6":"Port Credit", "16":"Edmonton Jasper", "11":"Calgary Shaganappi", "1029":"Calgary Britannia" }
-
-    #declaring dictionaries for sum of store sales and same store sales
     store_sales = {}
     same_store_sales = {}
     same_store_transactions = {}
@@ -55,20 +44,20 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
     html2 = """\
 
     <br>
-    <table border="1" bgcolor="#fff2cc" font face="Calibri" font size=10 bordercolor="#000000" style="min-width": 1000px; width: 100%;" width="100%"><font color="#FFFFFF"><tr>
-    <th bgcolor="#8c2830" class="mobile_td"><font color = "#FFFFFF">Store</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Yesterday</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Last Year</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Transactions</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Transactions LY</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">WTD</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">WTD LY</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Average Sale</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Average Sale LY</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th></tr></font>
+    <table border="1" bgcolor="#E7E6E6" font face="Calibri" font size=10 bordercolor="#000000" style="min-width": 1000px; width: 100%;" width="100%"><font color="#FFFFFF"><tr>
+    <th bgcolor="2F75B5" class="mobile_td"><font color = "#FFFFFF">Store</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Yesterday</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Last Year</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Transactions</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Transactions LY</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">WTD</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">WTD LY</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Average Sale</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Average Sale LY</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th></tr></font>
     """
     lastyear = ""
     yesterday = ""
@@ -85,25 +74,23 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
 
     for row in csv_reader:
 
-            #generates the names of the stores
-            html2 += """<tr><td width="150">"""
+            html2 += """<tr><td width="130">"""
             html2 += str(stores[row[0]])
             html2 += """</td>"""
 
-            #Yesterday's sales
             if row[4] == "":
                 yesterday = '0'
             else:
                 yesterday = round(float(row[4]),2)
                 store_sales.update({stores[row[0]] : yesterday})
-                html2 += """<td width="80">"""
+                html2 += """<td width="80" align="center">"""
                 html2 += """$""" + str(format(yesterday, ','))
                 html2 += """</td>"""
 
             #Last year's sales
             if row[8] == "":
                 lastyear = '0'
-                html2 += """<td width="80">"""
+                html2 += """<td width="80" align="center">"""
                 html2 += """$""" + str(lastyear)
                 html2 += """</td>"""
             else:
@@ -111,7 +98,7 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
                 #testing for same store
                 same_store_sales.update({stores[row[0]] : yesterday})
                 store_sales_last_year.update({stores[row[0]] : lastyear})
-                html2 += """<td width="80">"""
+                html2 += """<td width="80" align="center">"""
                 html2 += """$""" + str(format(lastyear, ','))
                 html2 += """</td>"""
 
@@ -173,13 +160,13 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
             if row[15] == "":
                 wtd = '0'
                 store_wtd.update({stores[row[0]] : wtd})
-                html2 += """<td width="75">"""
+                html2 += """<td width="75" align=center">"""
                 html2 += """100%"""
                 html2 += """</td>"""
             else:
                 wtd = round(float(row[15]), 2)
                 store_wtd.update({stores[row[0]] : wtd})
-                html2 += """<td width="75">"""
+                html2 += """<td width="75" align="center">"""
                 html2 += """$""" + str(format(wtd, ','))
                 html2 += """</td>"""
 
@@ -187,7 +174,7 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
             if row[19] == "":
                 wtdLastYear = int('0')
                 store_wtd_ly.update({stores[row[0]] : wtdLastYear})
-                html2 += """<td width="75">"""
+                html2 += """<td width="75" align="center">"""
                 html2 += """0"""
                 html2 += """</td>"""
             else:
@@ -195,7 +182,7 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
                 #same store
                 same_store_wtd.update({stores[row[0]] : wtd})
                 store_wtd_ly.update({stores[row[0]] : wtdLastYear})
-                html2 += """<td width="75">"""
+                html2 += """<td width="75" align="center">"""
                 html2 += """$""" + str(format(wtdLastYear, ','))
                 html2 += """</td>"""
 
@@ -248,16 +235,18 @@ with open('C:/users/aphaneuf/Desktop/daily_sales.txt') as csv_file:
                 html2 += str(averageSaleDifference)+"""%"""
                 html2 += """</td>"""
 
-            html2 += """</tr></table>
-            </p>
-            </body>
-            </html>
-
-        """.format(**locals())"""
+            html2 += """</tr>"""
 
 # Create the body of the message (a plain-text and an HTML version).
 text = ""
-#sum of store sales and same store variables
+html2 += """
+    </table>
+    </p>
+    </body>
+    </html>
+
+""".format(**locals())
+
 storeSalesYesterday = round(sum(store_sales.values()),2)
 storeSalesLastYear = round(sum(store_sales_last_year.values()),2)
 storeDifference = round(float((100*(storeSalesYesterday-storeSalesLastYear)/storeSalesLastYear)),2)
@@ -306,48 +295,48 @@ html = """\
     </style>
     </head>
     <body class="mobile">
-    <table border="1" bgcolor="#fff2cc" font-face="Calibri" font size=10 bordercolor="#000000" width="100%"><font color="#FFFFFF"><tr>
-    <th class="mobile_td" bgcolor="#8c2830"><font color = "#FFFFFF">Store</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Yesterday</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Last Year</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Transactions</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Transactions LY</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">WTD</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">WTD LY</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Average Sale</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Average Sale LY</font></th>
-    <th bgcolor="#8c2830"><font color = "#FFFFFF">Difference</font></th></tr></font>
+    <table border="1" bgcolor="#E7E6E6" font-face="Calibri" font size=10 bordercolor="#000000" width="100%"><font color="#FFFFFF"><tr>
+    <th class="mobile_td" bgcolor="2F75B5"><font color = "#FFFFFF">Store</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Yesterday</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Last Year</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Transactions</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Transactions LY</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">WTD</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">WTD LY</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Average Sale</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Average Sale LY</font></th>
+    <th bgcolor="2F75B5"><font color = "#FFFFFF">Difference</font></th></tr></font>
     <tr>
-    <td>Planet Organic Market</td>
-    <td>${storeSalesYesterday:,}</td>
-    <td width="80">${storeSalesLastYear:,}</td>
+    <td width="130">Planet Organic Market</td>
+    <td width="80" align="center">${storeSalesYesterday:,}</td>
+    <td width="80" align="center">${storeSalesLastYear:,}</td>
     <td width="75" align="center">{storeDifference}%</td>
     <td width="75" align="center">{storeTransactions}</td>
     <td width="75" align="center">{storeTransactionsLastYear}</td>
     <td width="75" align="center">{storeTransactionsDifference}%</td>
-    <td>${storeWtd:,}</td>
-    <td>${storeWtdLastYear:,}</td>
-    <td align="center">{storeWtdDifference}%</td>
-    <td align="center">${storeAverageSale:,}</td>
-    <td align="center">${storeAverageSaleLastYear:,}</td>
-    <td align="center">{storeAverageSaleDifference}%</td>
+    <td width="75" align="center">${storeWtd:,}</td>
+    <td width="75" align="center">${storeWtdLastYear:,}</td>
+    <td width="75" align="center">{storeWtdDifference}%</td>
+    <td width="75" align="center">${storeAverageSale:,}</td>
+    <td width="75" align="center">${storeAverageSaleLastYear:,}</td>
+    <td width="75" align="center">{storeAverageSaleDifference}%</td>
     </tr><tr>
-    <td>Same Store</td>
-    <td>${sameStoreSales:,}</td>
-    <td>${sameStoreSalesLastYear:,}</td>
-    <td align="center">{sameStoreDifference}%</td>
-    <td align="center">{sameStoreTransactions}</td>
-    <td align="center">{storeTransactionsLastYear}</td>
-    <td align="center">{sameStoreTransactionDifference}%</td>
-    <td>${sameStoreWtd:,}</td>
-    <td>${storeWtdLastYear:,}</td>
-    <td align="center">{sameStoreWtdDifference}</td>
-    <td align="center">${sameStoreAverageSale}</td>
-    <td align="center">${storeAverageSaleLastYear}</td>
-    <td align="center">{sameStoreAverageSaleDifference}%</td>
+    <td width="130">Same Store</td>
+    <td width="80" align="center">${sameStoreSales:,}</td>
+    <td width="80" align="center">${sameStoreSalesLastYear:,}</td>
+    <td width="75" align="center">{sameStoreDifference}%</td>
+    <td width="75" align="center">{sameStoreTransactions}</td>
+    <td width="75" align="center">{storeTransactionsLastYear}</td>
+    <td width="75" align="center">{sameStoreTransactionDifference}%</td>
+    <td width="75" align="center">${sameStoreWtd:,}</td>
+    <td width="75" align="center">${storeWtdLastYear:,}</td>
+    <td width="75" align="center">{sameStoreWtdDifference}</td>
+    <td width="75" align="center">${sameStoreAverageSale}</td>
+    <td width="75" align="center">${storeAverageSaleLastYear}</td>
+    <td width="75" align="center">{sameStoreAverageSaleDifference}%</td>
     </table>
     """ .format(**locals())
 
@@ -365,8 +354,8 @@ msg.attach(part1)
 msg.attach(part2)
 
 # Send the message via local SMTP server.
-s = smtplib.SMTP('smtp.telus.net')
+s = smtplib.SMTP('smtp.host.com')
 # sendmail function takes 3 arguments: sender's address, recipient's address
 # and message to send - here it is sent as one string.
-s.sendmail(me, you, msg.as_string())
+s.sendmail(message_from, message_to, msg.as_string())
 s.quit()
